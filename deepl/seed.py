3aea6feb6679aed749ec8425134d97c19d70de11
pufferandrr <1075967830@qq.com>
2025-06-01 18:52:49 +0800

    seed

---
+++ deepl/seed.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+import random
+seed = 42
+torch.manual_seed(seed)# 设置随机种子
+np.random.seed(seed)# 设置随机种子
+random.seed(seed)# 设置随机种子
+
+
+
+
+
+
